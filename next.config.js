9459a49d3fa08b9fee02bab95330b2bb67d74371
gaussianprime <wdai418@gmail.com>
2021-11-21 23:58:57 -0600

    sddsa

--- next.config.js
+++ next.config.js
@@ -1,28 +1,13 @@
-
 module.exports = {
-  
   // Prefer loading of ES Modules over CommonJS
-  experimental: {esmExternals: true},
+  experimental: { esmExternals: true },
   // Support MDX files as pages:
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
   // Support loading `.md`, `.mdx`
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-
-    // Example using webpack option
-    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+  webpack: (config, options) => {
     config.node = {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
-    }
-    return config
-},webpackDevMiddleware: config => {
-  // Perform customizations to webpack dev middleware config
-  // Important: return the modified config
-  return config
-},
+      fs: "empty",
+    };
+  },
   reactStrictMode: true,
-}
+};
