010271725aafdc00459822c7ea3378491502b877
gaussianprime <wdai418@gmail.com>
2021-11-21 23:56:15 -0600

    sa

--- next.config.js
+++ next.config.js
@@ -5,22 +5,24 @@ module.exports = {
   experimental: {esmExternals: true},
   // Support MDX files as pages:
   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-  // Support loading `.md`, `.mdx`:
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        // The default `babel-loader` used by Next:
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {/* jsxImportSource: …, otherOptions… */}
-        }
-      ]
-    })
+  // Support loading `.md`, `.mdx`
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
 
+    // Example using webpack option
+    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    config.node = {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
     return config
-  },
+},webpackDevMiddleware: config => {
+  // Perform customizations to webpack dev middleware config
+  // Important: return the modified config
+  return config
+},
   reactStrictMode: true,
 }
