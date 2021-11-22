04093bf15e5b1fd45fc3733723da3523d11f5800
gaussianprime <wdai418@gmail.com>
2021-11-22 00:18:53 -0600

    ds

--- next.config.js
+++ next.config.js
@@ -3,13 +3,9 @@ module.exports = {
   experimental: { esmExternals: true },
   // Support MDX files as pages:
   pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     // Fixes npm packages that depend on fs module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
+    config.node = { fs: 'empty' }
     return config;
   },
   reactStrictMode: true,
