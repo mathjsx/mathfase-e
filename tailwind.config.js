636d00a549fdc9380752e401512666b9a48b8752
gaussianprime <wdai418@gmail.com>
2021-11-23 17:09:21 -0600

    fas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   purge: [
     './src/**/*.html',
-    './src/**/*.{js, ts, tsx, mdx}',
+    './src/**/*.{js, ts, tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
