522833e6e29cfb81a69452161fd05710c7e7a9d9
gaussianprime <wdai418@gmail.com>
2021-11-25 17:55:58 -0600

    adsdsa

--- utils/submit.tsx
+++ utils/submit.tsx
@@ -40,7 +40,7 @@ const SingleForm: FC<{
     }
     
     return (
-        <div className={"h-24 w-24 rounded-xl bg-blue-200"}>
+        <div>
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                     Answer
